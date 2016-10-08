f9d33f2baad519960c2bccc78b32cbc18c300f76
kevin <oliverakevin@gmail.com>
2016-10-08 23:20:33 +0200

    Agregar archivos de proyecto.

---
+++ CRMFacilitoInicial2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CRMFacilitoInicial2.MvcApplication" Language="C#" %>
---
+++ CRMFacilitoInicial2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CRMFacilitoInicial2.Startup))]
+namespace CRMFacilitoInicial2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
